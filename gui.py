fc5a4147709d0edd632255325413966c9acf773f
Gilbert Topete <gtopete2004@csu.fullerton.edu>
2025-04-26 20:12:48 -0700

    Cleaned up unnecessary libraries, finished frontend and backend

--- gui.py
+++ gui.py
@@ -1,82 +1,135 @@
-from PyQt5 import QtWidgets, QtGui, uic
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
 import sys
 import os
-import kb_gen
+import time
+from kb_gen import kbArticleGenerator
 
-input_path = ''
-output_path = ''
-
-class MainWindow(QtWidgets.QDialog):
+#Pages
+class welcomeScreen(QtWidgets.QDialog):
     def __init__(self):
-        super(MainWindow, self).__init__()
-        uic.loadUi("screen1.ui", self)
+        super(welcomeScreen, self).__init__()
+        self.setWindowIcon(QtGui.QIcon('NXTKB.png'))
+        uic.loadUi("welcomeScreen.ui", self)
         self.button.clicked.connect(self.gotoscreen2)
 
     def gotoscreen2(self):
         widget.setCurrentIndex(widget.currentIndex()+1)
 
-class Screen2(QtWidgets.QDialog):
+class fileSelectionScreen(QtWidgets.QDialog):
     def __init__(self):
-        super(Screen2, self).__init__()
-        uic.loadUi("screen2.ui", self)
+        super(fileSelectionScreen, self).__init__()
+        uic.loadUi("fileSelectionScreen.ui", self)
 
-        self.x = LoadingScreen()
+        self.waiting.hide()
+        self.emptyOutputError.hide()
+        self.emptyInputError.hide()
+        self.button.setEnabled(True)
+
 
         self.inputBrowse.clicked.connect(self.browseInputFile)
         self.outputBrowse.clicked.connect(self.browseOutputFile)
-        self.button.clicked.connect(self.gotoloading)
+        self.button.clicked.connect(self.input_validation)
+        #self.button.clicked.connect(self.goToFinished)
 
     def browseInputFile(self):
         desktop_path = os.path.expanduser('~') + '/Desktop/'
-        input_file = QtWidgets.QFileDialog.getOpenFileName(self, 'Open input file', desktop_path, 'Text files (*.txt)')
-        self.x.input_path = input_file[0]
+        input_file = QtWidgets.QFileDialog.getOpenFileName(self, 'Open input file', desktop_path, 'XML files (*.xml)')
+        #self.input_path = input_file[0]
         self.inputFilePath.setText(input_file[0])
         
 
     def browseOutputFile(self):
         desktop_path = os.path.expanduser('~') + '/Desktop/'
-        output_file = QtWidgets.QFileDialog.getSaveFileName(self, 'Choose output location', desktop_path, 'Text files (*.txt)')
-        self.x.output_path = output_file[0]
+        output_file = QtWidgets.QFileDialog.getSaveFileName(self, 'Choose output location', desktop_path, 'JSON files (*.json)')
+        #self.output_path = output_file[0]
         self.outputFilePath.setText(output_file[0])
 
-    def gotoloading(self):
-        widget.setCurrentIndex(widget.currentIndex()+1)
-        self.x.kbGenerate()
+    def input_validation(self):
+        if not self.outputFilePath.text():
+            self.emptyOutputError.show()
+        else:
+            self.emptyOutputError.hide()
+        
+        if not self.inputFilePath.text():
+            self.emptyInputError.show()
+        else:
+            self.emptyInputError.hide()
+        
+        if self.inputFilePath.text() and self.outputFilePath.text():
+            self.kbGenerate(self.inputFilePath.text(), self.outputFilePath.text())
+            #self.goToFinished()
 
-class LoadingScreen(QtWidgets.QDialog):
-    def __init__(self):
-        super(LoadingScreen, self).__init__()
-        uic.loadUi('loading.ui', self)
-        self.input_path = ""
-        self.output_path = ""
-    
-    def kbGenerate(self):
-        if kb_gen.kbArticleGenerator(self.input_path, self.output_path):
-            widget.setCurrentIndex(widget.currentIndex()+1)
+    def kbGenerate(self, input, output):
+        self.waiting.show()
+        self.button.setEnabled(False)
+        
+        self.thread = QtCore.QThread()
+        self.worker = Worker(input, output)
+        self.worker.moveToThread(self.thread)
+
+        self.thread.started.connect(self.worker.run)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+
+        self.thread.start()
+        
+        self.thread.finished.connect(self.goToFinished)
+        
+
+    def goToFinished(self):
+        self.button.setEnabled(True)
+        self.waiting.hide()
+        self.inputFilePath.clear()
+        self.outputFilePath.clear()
+        widget.setCurrentIndex(widget.currentIndex()+1)
 
 class Finished(QtWidgets.QDialog):
     def __init__(self):
         super(Finished, self).__init__()
         uic.loadUi('finished.ui', self)
-        self.exit_button.clicked.connect(self.close)
         
+        self.exit_button.clicked.connect(self.close_window)
+        self.back_button.clicked.connect(self.backToFileSelection)
 
+    def close_window(self):
+        for i in range(widget.count()):
+            widget.close()
 
+    def backToFileSelection(self):
+        widget.setCurrentIndex(widget.currentIndex()-1)
+
+#Worker class to run the KB article generation logic
+class Worker(QtCore.QObject):
+    finished = QtCore.pyqtSignal()
+    
+    def __init__(self, input_file, output_file):
+        super().__init__()
+        self.input_file = input_file
+        self.output_file = output_file
+    
+    def run(self):
+        kbArticleGenerator(self.input_file, self.output_file)
+        time.sleep(5)
+        self.finished.emit()
+
+#Starting the program
 app = QtWidgets.QApplication(sys.argv)
 widget = QtWidgets.QStackedWidget()
 
-mainwindow = MainWindow()
-screen2 = Screen2()
-#loading_screen = LoadingScreen()
+welcome_screen = welcomeScreen()
+file_selection_screen = fileSelectionScreen()
 finished = Finished()
 
-widget.addWidget(mainwindow)
-widget.addWidget(screen2)
-#widget.addWidget(loading_screen)
+widget.addWidget(welcome_screen)
+widget.addWidget(file_selection_screen)
 widget.addWidget(finished)
 
 widget.setFixedHeight(500)
 widget.setFixedWidth(500)
+
+widget.setWindowIcon(QtGui.QIcon('NXTKB.ico'))
+widget.setWindowTitle('NXTKB')
 
 widget.show()
 
--- kb_gen.py
+++ kb_gen.py
@@ -1,18 +1,71 @@
 from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+import xml.etree.ElementTree as ET
+import json
+
+
+#input_file = "report.xml"
+#output_file = "test_output.json"
 
 def kbArticleGenerator(input_file, output_file):
-    f = open(input_file, "r")
-    reply_chain = f.read()
+    template = """
+    From the following service ticket discussion, can you create a knowledgebase article
+    with its corresponding ticket number so we know where to place them?
+
+    If you cannot generate a knowledgebase article because there's too little information to go off of, 
+    you can safely ignore it.
+
+    Please replace any names found with 'user'.
+
+    Report: {service_ticket_report}
+    """
+    #Reads XML
+    tree = ET.parse(input_file)
+    xml_root = tree.getroot()
+    service_ticket_root = xml_root[0][0]
+    service_tickets = []
+    service_ticket_reports = []
+
+    #Gathers ticket numbers and their corresponding notes and places them in service_tickets
+    for service_ticket in service_ticket_root:
+        ticket_dict = {}
+        ticket_dict['ticketNumber'] = service_ticket.get('TicketNbr')
+        ticket_dict['ticketSummary'] = service_ticket.get('Summary')
+        ticket_notes = []
+        for notes in service_ticket[0][0][0]:
+            ticket_notes.append(notes.get('Textbox112')) #'Textbox112' refers to each note added to a service ticket
+        ticket_dict['ticketNotes'] = ticket_notes
+        service_tickets.append(ticket_dict)
+
+    #Stores ticket numbers and their corresponding ticket notes
+    for ticket in service_tickets:
+        service_ticket_report = ''''''
+        service_ticket_report += f'Start of ticket number {ticket.get('ticketNumber')}...\n\n'
+        service_ticket_report += f'Summary: {ticket.get('ticketSummary')}\n'
+        service_ticket_report += 'Discussion notes: \n'
+        for note in ticket.get('ticketNotes'):
+            service_ticket_report += f'{note} \n'
+        service_ticket_report += f"\nEnd of service ticket {ticket.get('ticketNumber')}...\n\n\n"
+        service_ticket_reports.append(service_ticket_report)
+        #print(service_ticket_report)
+
 
     model = OllamaLLM(model='llama3')
+    prompt = ChatPromptTemplate.from_template(template)
+    chain = prompt | model
 
-    kb_article = model.invoke(input=reply_chain)
+    for i in range(len(service_ticket_reports)):
+        kb_article = ''''''
+        kb_article += chain.invoke({'service_ticket_report': service_ticket_reports[i]})
+        kb_article += '\n\n\n\n'
+        service_tickets[i]['kbArticle'] = kb_article
+        del service_tickets[i]['ticketNotes'] #Removes notes from JSON, as we're only focused on the KB articles
 
-    f = open(output_file, "w")
-    f.write(kb_article)
-    f.close()
+    with open(output_file, "w") as json_file:
+        json.dump(service_tickets, json_file, indent=4)
 
     print("KB article successfully generated!")
-
     return True
 
+#kbArticleGenerator(input_file, output_file)
